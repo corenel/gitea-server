5457d64fd5212f17cc7a4d8b0991cde9d08013ab
corenel <xxdsox@gmail.com>
2019-08-17 12:25:25 +0800

    Use explict token in url for clong private repos

--- migrate.py
+++ migrate.py
@@ -11,15 +11,13 @@ import setting
 if __name__ == '__main__':
     session = requests.Session()  # Gitea
     session.headers.update({
-        "Content-type":
-        "application/json",
-        "Authorization":
-        "token {0}".format(setting.gitea_token),
+        'Content-type': 'application/json',
+        'Authorization': f'token {setting.gitea_token}',
     })
 
-    r = session.get("{0}/user".format(setting.gitea_url))
+    r = session.get(f'{setting.gitea_url}/user')
     if r.status_code != 200:
-        print("Cannot get user details", file=sys.stderr)
+        print('Cannot get user details', file=sys.stderr)
         exit(1)
 
     gitea_uid = json.loads(r.text)["id"]
@@ -30,23 +28,30 @@ if __name__ == '__main__':
         # Mirror to Gitea if I haven't forked this repository from elsewhere
         # if not repo.fork:
         if True:
-            print('migrating {}'.format(repo.full_name))
+            print(f'migrating {repo.full_name} ({repo.clone_url})')
             m = {
-                "repo_name": repo.full_name.replace("/", "-"),
-                "description": repo.description or "not really known",
-                "clone_addr": repo.clone_url,
-                "mirror": True,
-                "private": repo.private,
-                "uid": gitea_uid,
+                'repo_name':
+                    repo.full_name.replace('/', '-'),
+                'description':
+                    repo.description or "not really known",
+                'clone_addr':
+                    repo.clone_url.replace(
+                        '//', f'//{setting.github_token}:x-oauth-basic@'),
+                'mirror':
+                    True,
+                'private':
+                    repo.private,
+                'uid':
+                    gitea_uid,
             }
 
-            if repo.private:
-                m["auth_username"] = setting.github_username
-                m["auth_password"] = "{0}".format(setting.github_token)
+            # if repo.private:
+            #     m["auth_username"] = setting.github_username
+            #     m["auth_password"] = "{0}".format(setting.github_token)
 
             jsonstring = json.dumps(m)
 
-            r = session.post("{0}/repos/migrate".format(setting.gitea_url),
+            r = session.post(f'{setting.gitea_url}/repos/migrate',
                              data=jsonstring)
             if r.status_code != 201:  # if not CREATED
                 if r.status_code == 409:  # repository exists
--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [[ ! -d gitea ]]; then
+  mkdir gitea
+fi
+
+if [[ ! -d postgres ]]; then
+  mkdir postgres
+fi
+
 USER_UID=${UID} USER_GID=${GID} docker-compose up
-
-
